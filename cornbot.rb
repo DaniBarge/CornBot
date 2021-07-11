f4da7049e38782ddade0ed41089fd3969b79c3d3
Dani Barge <danielle.barge@live.co.uk>
2021-07-11 16:54:19 +0100

    Updated text function

--- cornbot.rb
+++ cornbot.rb
@@ -3,8 +3,8 @@ require 'dotenv/load'
 
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
+bot.message(with_text: 'pop') do |event|
+  event.respond 'corn'
 end
 
 bot.message(containing: "🌽") do |event|
