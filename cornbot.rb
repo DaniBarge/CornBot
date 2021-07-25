aab6d6ef785b60370f93a94c3709c2689dfb059a
Dani Barge <danielle.barge@live.co.uk>
2021-07-25 22:14:45 +0100

    Added Speedwagon response.

--- cornbot.rb
+++ cornbot.rb
@@ -3,12 +3,16 @@ require 'dotenv/load'
 
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 
-bot.message(with_text: 'pop') do |event|
+bot.message(with_text: ["pop", "Pop", "POP"]) do |event|
   event.respond 'corn'
 end
 
 bot.message(containing: "🌽") do |event|
   event.respond File.readlines("cornfacts.txt").sample
+end
+
+bot.message(containing: ["Speedwagon", "speedwagon", "speed wagon", "Speed Wagon", "SPEEDWAGON", "Speed wagon"]) do |event|
+  event.respond File.readlines("speedwagon.txt").sample
 end
 
 bot.run
